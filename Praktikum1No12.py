24972d3d57db3fade8d770b827c55aee56bb0b78
MAqilSadik <aqilsadik@users.noreply.github.com>
2020-10-30 20:34:15 +0700

    Create Praktikum1No12.py

---
+++ Praktikum1No12.py
@@ -0,0 +1,19 @@
+a = 8
+b = 3
+if (a>0) and (b>0):
+    print("keduanya positif")
+else:
+    print("keduanya positif")
+
+
+print("======================")
+
+a = 8
+b = 3
+if (a>0):
+    if (b>0):
+        print("keduanya positif")
+    else:
+        print("keduanya tidak positif")
+else:
+    print("keduanya tidak positif")
